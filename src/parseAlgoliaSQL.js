84f46f6226834f5dc15cf34d748b46e4b07f801b
Zuzana Štětinová <40138689+stetizu1@users.noreply.github.com>
2024-06-03 17:50:22 +0200

    Fix: Add basic NOT to query (#8)
    * Fix parseAlgolia if NOT is present
    
    * update readme

--- src/parseAlgoliaSQL.js
+++ src/parseAlgoliaSQL.js
@@ -1,7 +1,7 @@
 const parser = require('../algoliaDSLParser')
 
 const buildSearchExpression = (rule, db) => {
-  const { OR, AND } = db
+  const { OR, AND, NOT } = db
   const { token, key, value, left, right } = rule
   if (token === 'MATCH') {
     return `${key}:${value.value}`
@@ -13,6 +13,17 @@ const buildSearchExpression = (rule, db) => {
     const leftExpression = buildSearchExpression(left, db)
     const rightExpression = buildSearchExpression(right, db)
     return AND(leftExpression, rightExpression)
+  } else if (token === 'NOT') {
+    if (value.token === 'MATCH') {
+      const expression = buildSearchExpression(value, db)
+      return NOT('*', expression)
+    }
+    // Negation of combined filters is not supported by Algolia
+    // https://www.algolia.com/doc/guides/managing-results/refine-results/filtering/in-depth/combining-boolean-operators/#negate-combined-filters
+    throw new Error('NOT only supports MATCH')
+  } else {
+    // Only supports MATCH, OR, AND, NOT
+    throw new Error('UNKNOWN TOKEN')
   }
 }
 
